ccc6826f3f9f2cf50d84c554c8a4e179757f83a6
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-06-30 12:00:04 -0600

    Update Button.js

--- src/components/calculator/button/Button.js
+++ src/components/calculator/button/Button.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './Button.css';
 
 function Button({
-  name, clickHandler, equals, orangee,
+    name, clickHandler, equals, orangee,
 }) {
-  const handleClick = () => {
-    clickHandler({ name });
-  };
-  const className = [
-    equals ? 'equals' : '',
-    orangee ? 'orangee' : '',
-  ];
+    const handleClick = () => {
+        clickHandler({ namee });
+    };
+    const className = [
+        equals ? 'equals' : '',
+        orangee ? 'orangee' : '',
+    ];
 
-  return (
-    <button className={className.join(' ').trim()} type="button" onClick={handleClick}>
-      {name}
-    </button>
-  );
+    return (
+        <button className={className.join(' ').trim()} type="button" onClick={handleClick}>
+            {name}
+        </button>
+    );
 }
 
 export default Button;
