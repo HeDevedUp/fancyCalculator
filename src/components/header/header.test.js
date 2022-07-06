ada8ee04a39de7f737a43ee9895b260661a2e301
masangana <alexmasangana@gmail.com>
2022-07-06 23:58:08 +0300

    Add tests files
    --Create header test
    --Create Quote test

---
+++ src/components/header/header.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Header from './header';
+
+const MockHeader = () => (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
+  
+  test('renders correctly', () => {
+    const tree = renderer
+      .create(<MockHeader />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
